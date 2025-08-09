e6f182af6a322070226d935fade1b3c1fa8a6737
Oliver Payne <olliepayne@me.com>
2025-08-08 20:15:24 -0700

    Scaffold

---
+++ src/components/Modal.tsx
@@ -0,0 +1,8 @@
+export default function Modal() {
+  return (
+    <div className="rounded-3xl bg-gray-800 w-fit p-5 text-white">
+      <p>Placeholder text</p>
+      {/* child components/props */}
+    </div>
+  )
+}
