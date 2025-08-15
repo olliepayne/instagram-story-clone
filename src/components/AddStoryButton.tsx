3e5f04499f9a08ffd086e7b3b068e392346fab15
Oliver Payne <olliepayne@me.com>
2025-08-14 23:23:29 -0700

    Create file and begin scaffolding

--- src/components/AddStoryButton.tsx
+++ src/components/AddStoryButton.tsx
@@ -2,6 +2,7 @@ export default function AddStoryButton () {
   return (
     <button className="w-[89px] h-[89px] border-4 border-gray-900 rounded-[50%] bg-red-500 cursor-pointer">
       {/* open modal w/ inputs for story upload */}
+      {/* Add accessibilty */}
     </button>
   )
 }
