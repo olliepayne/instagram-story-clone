349652a6ca0fe735ffe0f024652cb8ef0e811dec
Oliver Payne <olliepayne@me.com>
2025-08-15 23:46:46 -0700

    Add basic functionality

--- src/components/AddStoryButton.tsx
+++ src/components/AddStoryButton.tsx
@@ -1,6 +1,17 @@
-export default function AddStoryButton () {
+interface AddStoryButton {
+  handleAddStoryModalVisibility: (show: boolean) => void
+}
+
+export default function AddStoryButton({ handleAddStoryModalVisibility }: AddStoryButton) {
+  function closeAddStoryModal() {
+    handleAddStoryModalVisibility(true)
+  }
+
   return (
-    <button className="w-[89px] h-[89px] border-4 border-gray-900 rounded-[50%] bg-red-500 cursor-pointer">
+    <button
+      className="w-[89px] h-[89px] border-4 border-gray-900 rounded-[50%] bg-red-500 cursor-pointer"
+      onClick={closeAddStoryModal}
+    >
       {/* open modal w/ inputs for story upload */}
       {/* Add accessibilty */}
     </button>
