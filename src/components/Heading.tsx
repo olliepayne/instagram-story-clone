e08183c5d4da7a688270e09722c75169c594ce37
Oliver Payne <olliepayne@me.com>
2025-08-21 20:59:06 -0700

    Add children

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,16 +1,17 @@
-interface Heading {
+interface HeadingProps {
   level: "h1" | "h2" | "h3" | "h4"
+  children?: React.ReactNode | React.ReactNode[]
 }
 
-export default function Heading({ level }: Heading) {
+export default function Heading({ level, children }: HeadingProps) {
   switch (level) {
     case "h1":
-      return <h1></h1>
+      return <h1>{children}</h1>
     case "h2":
-      return <h2></h2>
+      return <h2>{children}</h2>
     case "h3":
-      return <h3></h3>
+      return <h3>{children}</h3>
     case "h4":
-      return <h4></h4>
+      return <h4>{children}</h4>
   }
 }
