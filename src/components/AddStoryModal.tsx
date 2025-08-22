d8c95368db23e3401fbeb31379139a37c9bb35df
Oliver Payne <olliepayne@me.com>
2025-08-21 21:29:34 -0700

    Remove ModalWrapper and refactor

--- src/components/AddStoryModal.tsx
+++ src/components/AddStoryModal.tsx
@@ -1,15 +1,23 @@
-import ModalWrapper from "./ModalWrapper"
-
-interface AddStoryModal {
+interface AddStoryModalProps {
   isOpen: boolean
+  handleAddStoryModalVisibility: (isOpen: boolean) => void
 }
 
-export default function AddStoryModal({ isOpen }: AddStoryModal) {
-  return (
-    <ModalWrapper isOpen={isOpen}>
-      <div>
-        
-      </div>
-    </ModalWrapper>
+export default function AddStoryModal({
+  isOpen,
+  handleAddStoryModalVisibility
+}: AddStoryModalProps) {
+  return isOpen ? (
+    <div className="rounded-3xl bg-gray-800 w-fit p-5 text-white">
+      <button
+        onClick={() => {
+          handleAddStoryModalVisibility(false)
+        }}
+      >
+        Close
+      </button>
+    </div>
+  ) : (
+    <></>
   )
 }
