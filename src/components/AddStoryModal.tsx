8c452acea01fd798503fc8eb4b3c597ebd439981
Oliver Payne <olliepayne@me.com>
2025-08-21 22:11:01 -0700

    Add basic image file upload handling

--- src/components/AddStoryModal.tsx
+++ src/components/AddStoryModal.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 interface AddStoryModalProps {
   isOpen: boolean
   handleAddStoryModalVisibility: (isOpen: boolean) => void
@@ -7,15 +9,36 @@ export default function AddStoryModal({
   isOpen,
   handleAddStoryModalVisibility
 }: AddStoryModalProps) {
+  // state to hold file
+  const [selectedImage, setSelectedImage] = useState<File | null>()
+  function handleSetSelectedImage(event: React.ChangeEvent<HTMLInputElement>) {
+    const fileList = event.target.files
+    if (fileList) {
+      const newSelectedImage = { ...fileList[0] }
+      setSelectedImage(newSelectedImage)
+    }
+  }
+
+  // function to upload the file to local storage
+
   return isOpen ? (
-    <div className="rounded-3xl bg-gray-800 w-fit p-5 text-white">
+    <div className="relative">
       <button
+        className="absolute top-4 right-4"
         onClick={() => {
           handleAddStoryModalVisibility(false)
         }}
       >
-        Close
+        X
       </button>
+      <div className="absolute top-1/2 left-1/2 rounded-3xl bg-[#171717] w-fit p-5 text-white">
+        <p>Select an image</p>
+        <input
+          type="file"
+          accept="img/png, img/jpeg"
+          onChange={handleSetSelectedImage}
+        />
+      </div>
     </div>
   ) : (
     <></>
