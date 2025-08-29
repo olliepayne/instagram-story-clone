251802dfe6fae32ae131ad628582179bd0f73357
Oliver Payne <olliepayne@me.com>
2025-08-28 22:53:30 -0700

    Refactor as part of AddStoryModal refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
 import AddStoryButton from "./components/AddStoryButton"
-import { useEffect, useState } from "react"
 
 function App() {
-  const [addStoryModalIsOpen, setAddStoryModalIsOpen] = useState(false)
-  useEffect(() => {
-    document.addEventListener("keydown", (keydown) => {
-      if (keydown.key === "Escape") {
-        handleAddStoryModalVisibility(false)
-      }
-    })
-  })
-
-  function handleAddStoryModalVisibility(isOpen: boolean) {
-    setAddStoryModalIsOpen(isOpen)
-  }
-
   return (
     <div className="py-4 px-4">
-      <AddStoryButton
-        handleAddStoryModalVisibility={handleAddStoryModalVisibility}
-      />
+      <AddStoryButton />
     </div>
   )
 }
